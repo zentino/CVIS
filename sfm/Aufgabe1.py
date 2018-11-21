6734001cf4938b18fdae968de614f2d27f2fa009
Unknown <sn-z777@outlook.com>
2018-11-21 20:19:48 +0100

    verify pose

--- sfm/Aufgabe1.py
+++ sfm/Aufgabe1.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import cv2
 import glob
-from Uebungsblatt8.siftdetector import detect_keypoints
+from siftdetector import detect_keypoints
 
 image_pathes = glob.glob('images/*.png')
 image_pairs = np.array([image_pathes[:2], image_pathes[2:]])
@@ -43,6 +43,7 @@ def goodmatches(matches, threshold, img1, img2, kp1, kp2, fname1, fname2):
             pts2.append(kp2[m.trainIdx].pt)
 
     good_matches = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None)
+    print "Anzahl good matches mit einem threshold von " + str(threshold) + " : " + str(len(good))
     cv2.imwrite("kp_images/" + fname1 + "_" + fname2 + "_threshold_" + str(threshold) + ".png", good_matches)
     return pts1, pts2
 
@@ -80,18 +81,26 @@ def check_3DPoints(objectPoints):
 
 def match_and_draw(img_pairs):
     for pathes in img_pairs:
+        filename_w_ext1 = os.path.basename(pathes[0])
+        filename_w_ext2 = os.path.basename(pathes[1])
+        filename1 = os.path.splitext(filename_w_ext1)[0]
+        filename2 = os.path.splitext(filename_w_ext2)[0]
         image1 = cv2.imread(pathes[0])
         image2 = cv2.imread(pathes[1])
         gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
         gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
 
-        [detected_keypoints1, descriptors1] = detect_keypoints(pathes[0], 5)
-        [detected_keypoints2, descriptors2] = detect_keypoints(pathes[1], 5)
-
+        descriptors1 = np.loadtxt(filename1 + "dp1.txt", dtype=float,delimiter=',')
+        descriptors2 = np.loadtxt(filename2 + "dp2.txt", dtype=float,delimiter=',')
+        detected_keypoints1 = np.loadtxt(filename1 + "kp1.txt",dtype=float,delimiter=',')
+        detected_keypoints2 = np.loadtxt(filename2 + "kp2.txt",dtype=float,delimiter=',')
+        print "Anzahl Keypoints " + filename1 + " : " + str(len(detected_keypoints1))
+        print "Anzahl Keypoints " + filename2 + " : " + str(len(detected_keypoints2))
         kp1_cv2 = to_cv2_kplist(detected_keypoints1)
         d1_cv2 = to_cv2_di(descriptors1)
         kp2_cv2 = to_cv2_kplist(detected_keypoints2)
         d2_cv2 = to_cv2_di(descriptors2)
+
 
         image_out1 = np.array([])
         image_out2 = np.array([])
@@ -100,10 +109,6 @@ def match_and_draw(img_pairs):
         kp_image1 = cv2.drawKeypoints(gray1, kp1_cv2, image_out1, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         kp_image2 = cv2.drawKeypoints(gray2, kp2_cv2, image_out2, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-        filename_w_ext1 = os.path.basename(pathes[0])
-        filename_w_ext2 = os.path.basename(pathes[1])
-        filename1 = os.path.splitext(filename_w_ext1)[0]
-        filename2 = os.path.splitext(filename_w_ext2)[0]
         # Bilder mit Keypoints abspeichern
         cv2.imwrite("kp_images/" + filename1 + "_kp.png",kp_image1)
         cv2.imwrite("kp_images/" + filename2 + "_kp.png",kp_image2)
@@ -142,32 +147,14 @@ def match_and_draw(img_pairs):
         # Punkte die die Epipolareinschränkung einhalten
         pts1 = pts1[mask.ravel() == 1]
         pts2 = pts2[mask.ravel() == 1]
-
+        np.savetxt(filename1+"FMatrix.txt",F,delimiter=',')
+        np.savetxt(filename1+"Points1.txt",pts1,delimiter=',')
+        np.savetxt(filename1+"Points2.txt",pts2,delimiter=',')
         lines = cv2.computeCorrespondEpilines(pts1.reshape(-1,1,2), 1, F)
         lines = lines.reshape(-1, 3)
         img5, img6 = drawlines(gray2, gray1, lines, pts2, pts1)
         cv2.imwrite("kp_images/" + filename1 + "_" + filename2 + "_epilines5.png", img5)
         cv2.imwrite("kp_images/" + filename1 + "_" + filename2 + "_epilines6.png", img6)
-        """ print(F)
-                print(pts1)
-                print(pts2)
-                # Aufgabe 2
-                fx = fy = 721.5
-                cx = 690.5
-                cy = 172.8
-                print("-----")
-                K = np.array([[fx, 0., cx], [0., fy, cy], [0., 0., 1.]])
-                Rt = np.hstack((np.eye(3), np.zeros((3, 1))))
-                P0 = K.dot(Rt)
-                print("-----")
-                E = K.T * np.mat(F) * K
-                R1, R2, t = cv2.decomposeEssentialMat(E)
-                print("-----")
-                P1 = verify_camerapose(P0, K, R1, R2, t, pts1.reshape(-1, 1, 2), pts2.reshape(-1, 1, 2))
-                print("-----")
-                pointcloud = cv2.triangulatePoints(P0, P1, pts1, pts2)
-                write_ply(filename1 + 'punktwolke.ply', pointcloud)"""
-
 
 
 match_and_draw(image_pairs)
--- sfm/Aufgabe2.py
+++ sfm/Aufgabe2.py
@@ -1,5 +1,10 @@
 import numpy as np
 import cv2
+import glob
+import os
+
+image_pathes = glob.glob('images/*.png')
+image_pairs = np.array([image_pathes[:2], image_pathes[2:]])
 
 ply_header = '''ply
 format ascii 1.0
@@ -10,13 +15,11 @@ property float z
 end_header 
 '''
 
-
 def write_ply(fn, verts):
     verts = verts.reshape(-1, 3)
     with open(fn, 'w') as f:
         f.write(ply_header % dict(vert_num=len(verts)))
         np.savetxt(f, verts, '%f %f %f')
-
 
 def verify_camerapose(P0,K, R1, R2, t, pts1, pts2):
 
@@ -39,30 +42,35 @@ def verify_camerapose(P0,K, R1, R2, t, pts1, pts2):
 def check_3DPoints(objectPoints):
     countPositiveDepth = 0
     for o in objectPoints.T:
-        print(o)
-        print(o[2])
         if o[2] >= 0:
             countPositiveDepth += 1
 
     return countPositiveDepth
 
 
-def reconstruction():
-    F = np.genfromtxt("FMatrix.txt",dtype=float,delimiter=',')
-    pts1 = np.loadtxt("pts1.txt",dtype=int, delimiter=',')
-    pts2 = np.loadtxt("pts2.txt",dtype=int, delimiter=',')
+def reconstruction(img_pairs):
+    for pathes in img_pairs:
+        filename_w_ext1 = os.path.basename(pathes[0])
+        filename_w_ext2 = os.path.basename(pathes[1])
+        filename1 = os.path.splitext(filename_w_ext1)[0]
+        filename2 = os.path.splitext(filename_w_ext2)[0]
+        F = np.loadtxt(filename1+"FMatrix.txt",dtype=float,delimiter=',')
+        pts1 = np.loadtxt(filename1+"Points1.txt",dtype=int, delimiter=',')
+        pts2 = np.loadtxt(filename1+"Points2.txt",dtype=int, delimiter=',')
+        fx = fy = 721.5
+        cx = 690.5
+        cy = 172.8
+        K = np.array([[fx, 0., cx], [0., fy, cy], [0., 0., 1.]])
+        Rt = np.hstack((np.eye(3), np.zeros((3, 1))))
+        P0 = K.dot(Rt)
+        E = K.T * np.mat(F) * K
+        R1, R2, t = cv2.decomposeEssentialMat(E)
+        P1 = verify_camerapose(P0, K, R1, R2, t, pts1.reshape(-1, 1, 2), pts2.reshape(-1, 1, 2))
+        pointcloud = cv2.triangulatePoints(P0, P1, np.array(pts1.reshape(-1, 1, 2),dtype=np.float), np.array(pts2.reshape(-1, 1, 2),dtype=np.float))
+        # 4D Punkt in 3D umwandeln
+        pointcloud = cv2.convertPointsFromHomogeneous(pointcloud.T)
+        write_ply(filename1+'punktwolke.ply', pointcloud)
 
-    fx = fy = 721.5
-    cx = 690.5
-    cy = 172.8
-    K = np.array([[fx, 0., cx], [0., fy, cy], [0., 0., 1.]])
-    Rt = np.hstack((np.eye(3), np.zeros((3, 1))))
-    P0 = K.dot(Rt)
-    E = K.T * np.mat(F) * K
-    R1, R2, t = cv2.decomposeEssentialMat(E)
-    P1 = verify_camerapose(P0, K, R1, R2, t, pts1.reshape(-1, 1, 2), pts2.reshape(-1, 1, 2))
-    pointcloud = cv2.triangulatePoints(P0, P1, np.array(pts1.reshape(-1, 1, 2),dtype=np.float), np.array(pts2.reshape(-1, 1, 2),dtype=np.float))
-    write_ply("KITTI11" + 'punktwolke.ply', pointcloud)
 
+reconstruction(image_pairs)
 
-reconstruction()
